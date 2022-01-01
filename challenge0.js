77f38a69fca43d8f12860253581c653bce26ea12
KORIR JAPHETH KIBET <korirjay@gmail.com>
2022-01-01 12:38:01 +0300

    Add files via upload
    Day 0 of personal challenges.
    code to statically enter a number and check if it is even or odd.
    I will build on this in subsequent days adding more functionality as well as styling.
    This is plain HTML and javascript.

---
+++ challenge0.js
@@ -0,0 +1,13 @@
+function runFunction()
+{
+let num=2;
+var result;
+if (num%2==0) {
+    result="even number";
+    
+}
+else{
+    result="odd number"
+}
+document.getElementById("run").textContent=result
+}
